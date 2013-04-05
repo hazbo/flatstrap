a7e7016c6a470b666487a51116eed841bcd24854
Joey Emery <joey.emery@performancehorizon.com>
2013-04-05 11:03:29 +0100

    Initial commit

---
+++ public/js/base.js
@@ -0,0 +1,16 @@
+/*	------------------------------------------------------
+
+	Base.less
+	
+	The master JS file.
+	
+	Written by Joey Emery.
+	
+	Contents:
+		--- Document ready
+	
+------------------------------------------------------ */
+
+/* ---		Document Ready		--- */
+$(document).ready(function() {
+});
